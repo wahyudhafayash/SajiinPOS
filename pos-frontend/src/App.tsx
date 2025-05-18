d46f2169e68aa01d8cdc0a5d35d4b15a23808316
fayash <wahyudhafayash@gmail.com>
2025-05-18 10:28:59 +0700

    add: bottom nav

--- pos-frontend/src/App.tsx
+++ pos-frontend/src/App.tsx
@@ -1,19 +1,18 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Auth, Orders } from "./pages";
 import Header from "./components/shared/Header";
 
-const App = () => {
+const App: React.FC = () => {
   return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/orders" element={<Orders />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </Router>
   );
 };
 
